3eb48efcbb784a1e9d1c3449b167c735a753c711
이태훈 <rhymint@gmail.com>
2025-01-08 19:57:15 +0900

    feat: TopComponent 구현

---
+++ src/components/Top/Line.tsx
@@ -0,0 +1,37 @@
+/** @jsxImportSource @emotion/react */
+import { HStack } from '../Stack';
+import {Text} from '../Text/Text';
+
+interface Props {
+  text: string;
+  emphasize?: string[];
+}
+
+export default function Line({text, emphasize = []}: Props) {
+  const getTextElements = ({text, emphasize = []}: Props) => {
+    if (emphasize.length === 0) return [text];
+
+    const regexPattern = new RegExp(`(${emphasize.join('|')})`, 'g');
+    return text.split(regexPattern).filter(Boolean);
+  };
+
+  const elements = getTextElements({text, emphasize});
+
+  return (
+    <HStack>
+      {elements.map((text, index) => {
+        return (
+          <Text
+            key={`${text}-${index}`}
+            size="subTitle"
+            textColor={emphasize.includes(text) ? 'black' : 'gray'}
+            style={{whiteSpace: 'pre'}}
+            aria-hidden={true}
+          >
+            {`${text}`}
+          </Text>
+        );
+      })}
+    </HStack>
+  );
+}
