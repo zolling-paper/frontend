1912cb24ed8fd617ffaca6a2dc5222b494cce571
이태훈 <rhymint@gmail.com>
2025-01-16 17:06:24 +0900

    fix: date 처리 방식 변경

--- src/pages/create/ShowDateStep.tsx
+++ src/pages/create/ShowDateStep.tsx
@@ -7,25 +7,24 @@ import {Input} from '@/components/Input';
 import {VStack} from '@/components/Stack';
 import Top from '@/components/Top/Top';
 import {YMD} from '@/types/model';
+import {dateToYMD, YMDtoDateString} from '@/utils/date';
 
 interface ShowDateStepProps {
   formData: BoardFormData;
   setFormData: (data: BoardFormData) => void;
+  onSubmit: () => void;
 }
 
-const now = {year: new Date().getFullYear(), month: new Date().getMonth() + 1, day: new Date().getDate()};
+export default function ShowDateStep({formData, setFormData, onSubmit}: ShowDateStepProps) {
+  const now = dateToYMD(new Date());
 
-export default function ShowDateStep({formData, setFormData}: ShowDateStepProps) {
   const handleDateChange = (date: YMD) => {
     setFormData({...formData, showDate: date});
   };
 
-  const getDateString = (date: YMD) => {
-    return `${date.year}년 ${String(date.month).padStart(2, '0')}월 ${String(date.day).padStart(2, '0')}일`;
-  };
-
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    onSubmit();
   };
 
   return (
@@ -35,7 +34,7 @@ export default function ShowDateStep({formData, setFormData}: ShowDateStepProps)
         <Top.Line text="날짜를 정해주세요" emphasize={['날짜']} />
       </Top>
       <form onSubmit={handleSubmit} css={{width: '100%', display: 'flex', flexDirection: 'column', gap: '2rem'}}>
-        <Input labelText="날짜" value={getDateString(formData.showDate)} disabled />
+        <Input labelText="날짜" value={YMDtoDateString(formData.showDate)} disabled />
         <DatePicker onChange={handleDateChange} initialDate={now} />
         <FixedBottomCTA type="submit">다음</FixedBottomCTA>
       </form>
--- src/pages/create/page.tsx
+++ src/pages/create/page.tsx
@@ -1,5 +1,5 @@
 /** @jsxImportSource @emotion/react */
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 
 import NameStep from './NameStep';
 import PasswordStep from './PasswordStep';
@@ -7,6 +7,8 @@ import ShowDateStep from './ShowDateStep';
 import {Header} from '@/components/Header/Header';
 import {VStack} from '@/components/Stack';
 import {YMD} from '@/types/model';
+import {dateToYMD, YMDtoDate} from '@/utils/date';
+import {useRequestPostBoard} from '@/hooks/useRequestPostBoard';
 
 export type Step = 'name' | 'password' | 'showDate';
 
@@ -17,20 +19,38 @@ export interface BoardFormData {
 }
 
 export default function CreatePage() {
-  const [step, setStep] = useState<Step>('name');
   const now = new Date();
+  const [step, setStep] = useState<Step>('name');
   const [formData, setFormData] = useState<BoardFormData>({
     name: '',
     password: '',
-    showDate: {year: now.getFullYear(), month: now.getMonth() + 1, day: now.getDate()},
+    showDate: dateToYMD(now),
   });
+
+  const {mutate: postBoard, isSuccess, data} = useRequestPostBoard();
+
+  const handleSubmit = () => {
+    postBoard({
+      name: formData.name,
+      password: formData.password,
+      showDate: YMDtoDate(formData.showDate),
+    });
+  };
+
+  useEffect(() => {
+    if (isSuccess) {
+      console.log('Mutation result:', data);
+    }
+  }, [isSuccess, data]);
 
   return (
     <VStack p="1.5rem" gap="1rem">
       <Header />
       {step === 'name' ? <NameStep formData={formData} setFormData={setFormData} setStep={setStep} /> : null}
       {step === 'password' ? <PasswordStep formData={formData} setFormData={setFormData} setStep={setStep} /> : null}
-      {step === 'showDate' ? <ShowDateStep formData={formData} setFormData={setFormData} /> : null}
+      {step === 'showDate' ? (
+        <ShowDateStep formData={formData} setFormData={setFormData} onSubmit={handleSubmit} />
+      ) : null}
     </VStack>
   );
 }
