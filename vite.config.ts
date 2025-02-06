7aedfb84aaf3855069e868fb12828982ae3a192a
이태훈 <rhymint@gmail.com>
2025-02-06 17:39:46 +0900

    chore: config 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import path from 'path'
+import path from 'path';
 
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import {defineConfig} from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -17,7 +17,9 @@ export default defineConfig({
       '@components': path.resolve(__dirname, './src/components'),
       '@hooks': path.resolve(__dirname, './src/hooks'),
       '@utils': path.resolve(__dirname, './src/utils'),
-      '@assets': path.resolve(__dirname, './src/assets')
-    }
-  }
-})
+      '@apis': path.resolve(__dirname, './src/apis'),
+      '@type': path.resolve(__dirname, './src/type'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+    },
+  },
+});
