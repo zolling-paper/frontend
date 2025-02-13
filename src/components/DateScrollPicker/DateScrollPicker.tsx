63eb67e543b9b8e178cca9113f1c468a2052aa36
이태훈 <rhymint@gmail.com>
2025-02-13 13:59:25 +0900

    style: 필요없는 코드 제거

--- src/components/DateScrollPicker/DateScrollPicker.tsx
+++ src/components/DateScrollPicker/DateScrollPicker.tsx
@@ -1,5 +1,5 @@
 /** @jsxImportSource @emotion/react */
-import {useCallback, useEffect, useState} from 'react';
+import {useEffect, useState} from 'react';
 
 import {Box} from '../Box';
 import {HStack} from '../Stack';
